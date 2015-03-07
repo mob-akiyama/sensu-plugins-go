d4dcd26f2ceb9f9f432109e1b193f8b90b05d5c0
Kyohei Akiyama <akiyama@mobcast.jp>
2015-03-07 15:04:00 +0900

    Sat Mar  7 15:04:00 JST 2015

--- ec2.go
+++ ec2.go
@@ -2,28 +2,51 @@ package main
 
 import (
   "fmt"
-  "os"
   "time"
   "flag"
+  "net/http"
+  "io/ioutil"
   "github.com/awslabs/aws-sdk-go/aws"
   "github.com/awslabs/aws-sdk-go/gen/cloudwatch"
 )
 
-var aws_access_key = flag.String("a", nil, "AWS API KEY")
-var aws_secret_key = flag.String("k", nil, "AWS API SECRET")
-var fetch_age      = flag.Int("f", nil, "How long ago to fetch metrics for")
-flag.Parse()
+var aws_access_key = flag.String("a", "", "AWS API KEY")
+var aws_secret_key = flag.String("k", "", "AWS API SECRET")
+var f      = flag.Int("f", 300, "How long ago to fetch metrics for")
+var fetch_age = time.Duration(*f) * time.Second
 
-var defaultRegion  = os.Getenv("EC2_REGION")
-var instanceId     = os.Getenv("EC2_INSTANCE_ID")
-var creds          = aws.Creds(aws_access_key, aws_secret_key, "")
-var cli            = cloudwatch.New(creds, defaultRegion, nil)
+func getInstanceID() string {
+    url := "http://169.254.169.254/latest/meta-data/instance-id"
+
+    resp, _ := http.Get(url)
+    defer resp.Body.Close()
+
+    byteArray, _ := ioutil.ReadAll(resp.Body)
+    return string(byteArray)
+}
+
+func getRegion() string {
+    url := "http://169.254.169.254/latest/meta-data/placement/availability-zone"
+
+    resp, _ := http.Get(url)
+    defer resp.Body.Close()
+
+    byteArray, _ := ioutil.ReadAll(resp.Body)
+    return string(byteArray[0:len(byteArray)-1])
+}
+
+var aws_region  = getRegion()
+var instance_id = getInstanceID()
+
+var creds = aws.Creds(*aws_access_key, *aws_secret_key, "")
+var cw    = cloudwatch.New(creds, aws_region, nil)
 var dimensionParam = &cloudwatch.Dimension{
     Name:  aws.String("InstanceId"),
-    Value: aws.String(instanceId),
+    Value: aws.String(instance_id),
 }
 
 func main() {
+    flag.Parse()
     fmt.Print(getCPUUtilization(), "\n")
     fmt.Print(getStatusCheckFailed(), "\n")
     fmt.Print(getStatusCheckFailed_Instance(), "\n")
@@ -33,55 +56,55 @@ func main() {
 func getCPUUtilization() float64 {
     mt := &cloudwatch.GetMetricStatisticsInput{
         Dimensions: []cloudwatch.Dimension{*dimensionParam},
-        StartTime:  time.Now().Add(-600 * time.Second),
+        StartTime:  time.Now().Add(fetch_age),
         EndTime:    time.Now(),
         MetricName: aws.String("CPUUtilization"),
         Namespace:  aws.String("AWS/EC2"),
         Period:     aws.Integer(300),
         Statistics: []string{"Average"},
     }
-    resp, _ := cli.GetMetricStatistics(mt)
+    resp, _ := cw.GetMetricStatistics(mt)
     return *resp.Datapoints[0].Average
 }
 
 func getStatusCheckFailed() float64 {
     mt := &cloudwatch.GetMetricStatisticsInput{
         Dimensions: []cloudwatch.Dimension{*dimensionParam},
-        StartTime:  time.Now().Add(-600 * time.Second),
+        StartTime:  time.Now().Add(fetch_age),
         EndTime:    time.Now(),
         MetricName: aws.String("StatusCheckFailed"),
         Namespace:  aws.String("AWS/EC2"),
         Period:     aws.Integer(300),
         Statistics: []string{"Average"},
     }
-    resp, _ := cli.GetMetricStatistics(mt)
+    resp, _ := cw.GetMetricStatistics(mt)
     return *resp.Datapoints[0].Average
 }
 
 func getStatusCheckFailed_Instance() float64 {
     mt := &cloudwatch.GetMetricStatisticsInput{
         Dimensions: []cloudwatch.Dimension{*dimensionParam},
-        StartTime:  time.Now().Add(-600 * time.Second),
+        StartTime:  time.Now().Add(fetch_age),
         EndTime:    time.Now(),
         MetricName: aws.String("StatusCheckFailed_Instance"),
         Namespace:  aws.String("AWS/EC2"),
         Period:     aws.Integer(300),
         Statistics: []string{"Average"},
     }
-    resp, _ := cli.GetMetricStatistics(mt)
+    resp, _ := cw.GetMetricStatistics(mt)
     return *resp.Datapoints[0].Average
 }
 
 func getStatusCheckFailed_System() float64 {
     mt := &cloudwatch.GetMetricStatisticsInput{
         Dimensions: []cloudwatch.Dimension{*dimensionParam},
-        StartTime:  time.Now().Add(-600 * time.Second),
+        StartTime:  time.Now().Add(fetch_age),
         EndTime:    time.Now(),
         MetricName: aws.String("StatusCheckFailed_System"),
         Namespace:  aws.String("AWS/EC2"),
         Period:     aws.Integer(300),
         Statistics: []string{"Average"},
     }
-    resp, _ := cli.GetMetricStatistics(mt)
+    resp, _ := cw.GetMetricStatistics(mt)
     return *resp.Datapoints[0].Average
 }
