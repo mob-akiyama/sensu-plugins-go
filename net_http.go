868a2955349c4e431f8b06c2a8c954c966fc20e4
Kyohei Akiyama <akiyama@mobcast.jp>
2015-03-07 14:29:50 +0900

    Sat Mar  7 14:29:50 JST 2015

--- net_http.go
+++ net_http.go
@@ -4,12 +4,27 @@ import "fmt"
 import "net/http"
 import "io/ioutil"
 
+func getInstanceID() string {
+    url := "http://169.254.169.254/latest/meta-data/instance-id"
+  
+    resp, _ := http.Get(url)
+    defer resp.Body.Close()
+  
+    byteArray, _ := ioutil.ReadAll(resp.Body)
+    return string(byteArray)
+}
+
+func getRegion() string {
+    url := "http://169.254.169.254/latest/meta-data/placement/availability-zone"
+  
+    resp, _ := http.Get(url)
+    defer resp.Body.Close()
+  
+    byteArray, _ := ioutil.ReadAll(resp.Body)
+    return string(byteArray)
+}
+
 func main() {
-  url := "http://169.254.169.254/latest/meta-data/instance-id"
-
-  resp, _ := http.Get(url)
-  defer resp.Body.Close()
-
-  byteArray, _ := ioutil.ReadAll(resp.Body)
-  fmt.Println(string(byteArray)) // htmlをstringで取得
+    fmt.Println(getInstanceID())
+    fmt.Println(getRegion())
 }
